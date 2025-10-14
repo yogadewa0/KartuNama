0093825ae9bef7e0ad3a3fca4d9f7e8107534fc0
Neville Jeremy <nevillejro.laia@gmail.com>
2025-10-15 03:28:20 +0700

    bugfix: import lib email icon

--- app/src/main/java/com/example/utskartunama/MainActivity.kt
+++ app/src/main/java/com/example/utskartunama/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.Icon
