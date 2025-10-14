6913bce28d4967c2f2287b77d3635734892a3451
yogadewa0 <yogadewa0@gmail.com>
2025-10-14 15:48:52 +0700

    Feats: Block Image untuk profile picture

--- app/src/main/java/com/example/utskartunama/MainActivity.kt
+++ app/src/main/java/com/example/utskartunama/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.utskartunama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.utskartunama.ui.theme.UTSKartuNamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +32,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    KartuNama(
 
+                    )
                 }
             }
         }
@@ -29,17 +42,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun KartuNama(name: String,
+              title: String,
+              modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize().
+        background(colorResource(R.color.mint)).
+        padding(horizontal = 24.dp, vertical = 16.dp)
+    ) {
+        // Upper main page (Profile Pic, Full Name, Title)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            // Image black background
+            Box(modifier = Modifier
+                    .size(95.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(colorResource(R.color.dark_tile)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.android_logo),
+                    contentDescription = null,
+                    modifier = Modifier.size(65.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun KartuNamaPreview() {
     UTSKartuNamaTheme {
-        Greeting("Android")
+        KartuNama(
+
+        )
     }
 }
