28a9066cdb060ac2e42241b553c10e29d6d37579
yogadewa0 <yogadewa0@gmail.com>
2025-10-14 15:58:30 +0700

    Feats: Update MainActivity.kt, menambahkan text untuk menampilkan Nama

--- app/src/main/java/com/example/utskartunama/MainActivity.kt
+++ app/src/main/java/com/example/utskartunama/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,8 +21,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.utskartunama.ui.theme.UTSKartuNamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -70,6 +74,13 @@ fun KartuNama(name: String,
                     modifier = Modifier.size(65.dp)
                 )
             }
+            Text(
+                text = name,
+                fontSize = 38.sp,
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Center,
+                color = colorResource(R.color.stone_black)
+            )
         }
 
         // Bottom Contact list
