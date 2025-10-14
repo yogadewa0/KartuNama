427e93c87909ff7cfd2ca8276dc26c6f05d8c5fe
yogadewa0 <yogadewa0@gmail.com>
2025-10-14 14:08:26 +0700

    Edit function onCreate untuk persiapan view, dan hapus beberapa import tidak perlu

--- app/src/main/java/com/example/utskartunama/MainActivity.kt
+++ app/src/main/java/com/example/utskartunama/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.utskartunama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,14 +15,13 @@ import com.example.utskartunama.ui.theme.UTSKartuNamaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             UTSKartuNamaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
                 }
             }
         }
